bdac9acb5daab354c8d14820f5e272b79535f790
Parth Ganeriwala <ganeriwalaparth@gmail.com>
2024-03-26 23:29:42 -0400

    update for github page deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const optimize = require("next/dist/server/optimize-amp");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/portfolio",
   output: "export",  // <=== enables static exports
+  images: {
+    unoptimized: true
+  },
   reactStrictMode: true,
+
 };
 
 module.exports = nextConfig;
---
+++ out/_next/static/GLEB7t_49WRekRzjDWgvW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0-8196ea8097f5e7a0.js","static/chunks/pages/index-b8c61dac986b7516.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
